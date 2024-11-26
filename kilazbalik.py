5321a57b6e8d382e9d4118c719a0aad3084e8dae
KUNF24 <kunf5616@gmail.com>
2024-11-26 19:54:14 +0700

    Update kilazbalik.py

--- kilazbalik.py
+++ kilazbalik.py
@@ -82,8 +82,8 @@ while True:
         addr = (str(ip),int(port))
         for x in range(times):
            s.sendto(data,addr)
-	   print("\033[95m[\033[97m+\033[95m] \033[91mAttack  \033[33mMasive:::... " +ip+ " \033[0m")
-	   print("\033[95m[\033[97m+\033[95m] \033[97mSent:::::::::::::........... " +ip+ " \033[0m")
+	   print("\033[95m[\033[97m+\033[95m] \033[91mTarget:::::::::::........... " +ip+ " \033[0m")
+	   print("\033[95m[\033[97m+\033[95m] \033[97mIs being:::::::::::::....... " +ip+ " \033[0m")
 	   print("\033[95m[\033[97m+\033[95m] \033[92mLocked:::::::::::........... " +ip+ " \033[0m")
     except:
 	   print("\033[33m[\033[95m!\033[33m] Error!")
