d7d4329e61d64dc62e49afb62cdd671d088848cc
KUNF24 <kunf5616@gmail.com>
2024-12-22 08:29:07 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,7 @@ c = input(">>>: ")
 if c == "0":
     os.system("apt-get install python")
     os.system("apt-get install python2")
-    os.system("apt-get fade")
-    os.system("pip install request")
 elif c == "1":
     os.system("apt-get install python")
     os.system("apt-get install python2")
-    os.system("apt-get install fade")
-    os.system("pip install requests")
 print("Done.")
